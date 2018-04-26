312d1ef5ca55d8706359885efc221ac99047d824
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-04-26 19:19:44 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -83,6 +83,7 @@ def adding_matz
         }
      }
 
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 
 end
 
