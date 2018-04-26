5d0bac53d9e36131f421b3d30feaafcb7e5819cd
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-04-26 19:22:36 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,6 @@ def adding_matz
      }
 
 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
-
 end
 
 def changing_alan
@@ -106,7 +105,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 
-
+programmer_hash[:alan_kay] = alans_new_info
 end
 
 def adding_to_dennis
@@ -125,5 +124,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[dennis_ritchie][languages] << "Assembly"
 end
